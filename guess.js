a91880eab4037ae9305fd0bbacad50d27852de65
Rafael <rafaelnegretefonseca123@gmail.com>
2026-02-23 18:24:00 -0600

    pushing a lot of work. I created and finished my switch statement for different distances from guest.
     I also added a constant query selector for the message label so that I can change the colors.
    
    I added an event listener so when the enter key is pressed, the guessClick button runs.
    
    Added more comments.

--- guess.js
+++ guess.js
@@ -1,8 +1,16 @@
+/**
+ * Author: Rafael Negrete Fonseca
+ * Date: 2/23/2026
+ * GitHub URL: https://github.com/rnegrete01/js_hot_cold_game
+ */
+
 "use strict";
 
 // global variables
 let randomNum = 0;
 let tries = 0;
+const messageLabel = document.querySelector("#message");
+const guessInput = document.querySelector("#guess");
 
 // helper function
 const getRandomInt = (max = 100) => {
@@ -16,22 +24,65 @@ randomNum = getRandomInt();
 // event handler functions
 const guessClick = () => {
     const guess = parseInt(document.querySelector("#number").value);
-
     let message = "";
-    if (isNaN(guess)) {
-        message = "Not a valid number. Please enter a valid number."
-    } else if (guess < 1 || guess > 100) {
-        message = "Invalid number. Enter a number between 1 and 10.";
-    } else if (guess < randomNum) {
-        message = "Too small. Try again.";
-        tries++;
-    } else if (guess > randomNum) {
-        message = "Too big. Try again.";
-        tries++;
-    } else if (guess === randomNum) {
-        tries++;
-        const lastWord = (tries === 1) ? "try" : "tries";
-        message = `You guessed it in ${tries} ${lastWord}!`;
+
+    /**
+     * I nested my switch inside a breakme block and here's why:
+     * I couldn't figure out where to put the switch statement.
+     * I couldn't think of anywhere else to put it but inside the guess
+     * click button. I looked it up and figured out a way to break out
+     * of an 'if' statement so that I wouldn't continue to the switch if data invalid.
+     * Here is the link where I got the answer from (no AI like debbie said)
+     * URL: https://stackoverflow.com/questions/4851657/call-break-in-nested-if-statements
+     */
+
+    //Input Data Validation
+    breakme:{
+        if (isNaN(guess)) {
+            message = "Not a valid number. Please enter a valid number."
+            break breakme;
+        } else if (guess < 1 || guess > 100) {
+            message = "Invalid number. Enter a number between 1 and 100.";
+            break breakme;
+        }
+
+        let distance = randomNum - guess;
+        if (distance < 0) { //we need a positive number for distance. Negatives always smaller = bad.
+            distance *= -1;
+        }
+
+        let color = ""
+        switch (true) {
+            case (distance === 0):
+                const lastWord = (tries === 1) ? "try" : "tries";
+                message = `Bingo! You guessed it in ${tries} ${lastWord}!`;
+                color = "green";
+                break;
+            case (distance <= 5):
+                message = "Hot! You are within 5!"
+                color = "red"
+                break;
+            case (distance <= 10):
+                message = "Warmer!";
+                color = "orangered";
+                break;
+            case (distance <= 20):
+                message = "Warm!"
+                color = "orange"
+                break;
+            case (distance <= 30):
+                message = "Cold!"
+                color = "lightblue"
+                break;
+            case (distance <= 40):
+                message = "Colder!"
+                color = "blue"
+                break;
+            default:
+                color = "darkblue"
+                message = "Your way off!";
+        }
+        messageLabel.style.color = color
     }
     document.querySelector("#message").textContent = message;
 };
@@ -41,6 +92,7 @@ const playAgainClick = () => {
     tries = 0;
     document.querySelector("#number").value = "";
     document.querySelector("#message").textContent = "";
+    console.log(randomNum)
 };
 
 const bestScore = () => {}
@@ -49,6 +101,13 @@ const guessHistory = () => {}
 document.addEventListener("DOMContentLoaded", () => {
     document.querySelector("#guess").addEventListener(
         "click", guessClick);
+    // Enable "enter" key
+    document.querySelector("#number").addEventListener("keydown", (event) => {
+        if (event.key === "Enter") {
+            guessClick();
+        }
+    })
+
 });
 
 document.addEventListener("DOMContentLoaded", () => {
